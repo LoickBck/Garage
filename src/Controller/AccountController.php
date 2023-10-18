33ba1b4943038cb2425439790150c0a71e3b7531
LoickBck <124895725+LoickBck@users.noreply.github.com>
2023-10-18 10:03:37 +0200

    LoginError

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,27 +2,29 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Core\Exception\TooManyLoginAttemptsAuthenticationException;
 
 class AccountController extends AbstractController
 {
     /**
      * Permet à l'utilisateur de se connecter
      *
-     * @param AuthentificationUtils $utils
+     * @param AuthenticationUtils $utils
      * @return Response
      */
     #[Route('/login', name: 'account_login')]
-    public function index(AuthentificationUtils $utils): Response
+    public function index(AuthenticationUtils $utils): Response
     {
         $error = $utils->getLastAuthenticationError();
         $username = $utils->getLastUsername();
 
         $loginError = null;
 
-        if ($error instanceof TooManyLoginAttempsAuthenticationException)
+        if ($error instanceof TooManyLoginAttemptsAuthenticationException)
         {
             $loginError = 'Trop de tentatives de connexion. Réessayez plus tard.';
         }
